9159bdf4f178de37856d0fa477215af1e55dc47c
Cameron Taggart <cameron.taggart@gmail.com>
2023-08-25 00:03:48 +0200

    print source

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 use anyhow::Result;
-use clap::{Parser, Subcommand};
+use clap::Parser;
 
 #[derive(Parser)]
 #[command(version)]
 struct Cli {
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    Config { source: String, target: String },
+    source: String,
 }
 
 fn main() -> Result<()> {
-    let path = std::path::Path::new("/ev2");
+    let cli = Cli::parse();
+    let source = &cli.source;
+    println!("source: {source}");
+    let path = std::path::Path::new(&cli.source);
     let exists = path.exists();
     println!("exists: {exists}");
     let is_dir = path.is_dir();
