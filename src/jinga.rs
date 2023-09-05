01e710e10eeedd7a4d821b9fd77e611e842055f6
Cameron Taggart <cameron.taggart@gmail.com>
2023-09-05 16:02:58 +0200

    ipaddr and start ipsubnet

--- src/jinga.rs
+++ src/jinga.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeSet;
 use std::collections::HashMap;
-
+use std::net::IpAddr;
 use anyhow::anyhow;
 use dep_graph::{DepGraph, Node};
 use ipnet::IpNet;
@@ -40,7 +40,10 @@ fn create_env<'s>() -> Environment<'s> {
     env.set_undefined_behavior(minijinja::UndefinedBehavior::Strict);
     env.add_filter("string", string);
     env.add_filter("nthhost", nthhost);
+    env.add_filter("ipaddr", ipaddr);
+    env.add_filter("ipsubnet", ipsubnet);
     env
+    
 }
 
 fn create_ctx() -> Value {
@@ -132,9 +135,64 @@ fn nthhost(network: String, n: usize) -> Result<String, Error> {
     Ok(ip.to_string())
 }
 
+fn ipaddr(network: String, action: &Value) -> Result<String, Error> {
+    if let Some(action) = action.as_str() {
+        if let Ok(n) = action.parse::<usize>() {
+            return nthhost(network, n);
+        }
+        if action == "address" {
+            if let Ok(net) = IpNet::from_str(&network) {
+                return Ok(net.network().to_string());
+            }
+        }
+    }
+    Ok(network)
+}
+
+fn ipsubnet(ip: String, prefix_len: u8, incr: usize) -> Result<String, Error> {
+    let ip: IpAddr = IpAddr::from_str(&ip).map_err(|err| {
+        Error::new(ErrorKind::InvalidOperation, "cannot parse IP address").with_source(err)
+    })?;
+    // println!("ip: {}", ip);
+    let net = IpNet::new(ip, prefix_len).map_err(|err| {
+        Error::new(ErrorKind::InvalidOperation, "cannot create IP network").with_source(err)
+    })?;
+    // println!("net: {}", net);
+    // let hosts = net.hosts();
+    // let count = hosts.count();
+    // println!("count: {}", count);
+    // let i = hosts.into_iter().position(|host| host == ip);
+    // println!("i: {:?}", i);
+    // let n = n + i.unwrap_or(0);
+    // let network = net.network();
+    // print!("network: {}", network);
+
+    // let ip = if n == 0 {
+    //     net.network()
+    // } else {
+    //     println!("nthhost: {}", n);
+    //     net.hosts().nth(n - 1).ok_or(Error::new(
+    //         ErrorKind::InvalidOperation,
+    //         "cannot get nthhost",
+    //     ))?
+    // };
+    // println!("ip: {}", ip);
+    // let net = IpNet::new(ip, prefix_len).map_err(|err| {
+    //     Error::new(ErrorKind::InvalidOperation, "cannot create IP network").with_source(err)
+    // })?;
+    Ok(net.to_string())
+}
+
+
 #[cfg(test)]
 pub mod test {
     use super::*;
+
+    #[test]
+    fn test_ipsubnet() -> anyhow::Result<()> {
+        assert_render("{{ '100.73.7.30' | ipsubnet(28, 2) }}", "100.73.7.32/28")?;
+        Ok(())
+    }
 
     fn assert_render(tmpl_str: &str, expected: &str) -> anyhow::Result<()> {
         let env = create_env();
@@ -160,6 +218,19 @@ pub mod test {
     fn test_nthhost() -> anyhow::Result<()> {
         assert_render("{{ '10.0.0.0/8' | nthhost(0) }}", "10.0.0.0")?;
         assert_render("{{ '10.0.0.0/8' | nthhost(1) }}", "10.0.0.1")?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_ipaddr() -> anyhow::Result<()> {
+        assert_render("{{ '10.0.0.0/8' | ipaddr('0') }}", "10.0.0.0")?;
+        assert_render("{{ '10.0.0.0/8' | ipaddr('1') }}", "10.0.0.1")?;
+        assert_render("{{ '10.0.0.0/8' | ipaddr('address') }}", "10.0.0.0")?;
+        assert_render("{{ '10.0.0.0' | ipaddr('address') }}", "10.0.0.0")?;
+        assert_render(
+            "{{ '169.254.0.0/30' | ipaddr('2') | ipaddr('address') }}",
+            "169.254.0.2",
+        )?;
         Ok(())
     }
 }
